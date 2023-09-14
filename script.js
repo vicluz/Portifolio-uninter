a08d6ada5cbdbe7242d8a08118341dae99651bb8
vicluz <vicluz14@gmail.com>
2023-09-13 21:58:10 -0300

    resetando formulario

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-// Função para mostrar a mensagem de sucesso
+// Função para mostrar a mensagem de sucesso e redefinir o formulário
 function mostrarMensagemDeSucesso() {
     const mensagemSucesso = document.getElementById("mensagem-sucesso");
     mensagemSucesso.style.display = "block"; // Mostra a mensagem
+
+    // Redefine o formulário
+    const formulario = document.querySelector(".formulario-de-contato");
+    formulario.reset();
 }
 
 // Evento de envio do formulário
 document.querySelector(".formulario-de-contato").addEventListener("submit", function (event) {
     event.preventDefault(); // Impede o envio padrão do formulário
-    // Você pode adicionar aqui o código para enviar os dados para o backend, se necessário
+    
 
-    mostrarMensagemDeSucesso(); // Mostra a mensagem de sucesso
+    mostrarMensagemDeSucesso(); // Mostra a mensagem de sucesso e redefine o formulário
 });
